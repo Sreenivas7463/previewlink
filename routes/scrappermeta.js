5c23dce5af3eee99c8ab03218871821e32c9ef54
T. Sreenivasulu <59993636+Sreenivas7463@users.noreply.github.com>
2025-07-31 20:48:22 +0530

    Update scrappermeta.js

--- routes/scrappermeta.js
+++ routes/scrappermeta.js
@@ -20,11 +20,14 @@ router.get('/:url', function(req, resp) {
         var $ = cheerio.load(res.body)
         var title = $('meta[property="og:title"]').attr('content')
         var img = $('meta[property="og:image"]').attr('content')
+        var imgt = $('meta[property="twitter:image"]').attr('content')
         var url = $('meta[property="og:url"]').attr('content')
         var desc = $('meta[property="og:description"]').attr('content')
        var vid = $('meta[property="og:video:secure_url"]').attr('content')
+
+       var image = img ? img : imgt
         results.push({
-            title: title, img: img, url: urlv, description: desc, video: vid
+            title: title, img: imgage, url: urlv, description: desc, video: vid
         })
 
         //console.debug(results)
